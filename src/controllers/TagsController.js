feea6459eb5e94c6855c7e2eda7c9689b447c6fc
denilsonbaptista <baptistadenilsonbp@gmail.com>
2024-02-21 21:54:48 -0300

    add middleware

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -2,9 +2,9 @@ const knex = require('../database/knex');
 
 class TagsController {
   async index(request, response) {
-    const { user_id } = request.params;
+    const { note_id } = request.params;
 
-    const tags = await knex('movie_tags').where({ user_id });
+    const tags = await knex('movie_tags').where({ note_id });
 
     return response.status(200).json({ ...tags });
   }
